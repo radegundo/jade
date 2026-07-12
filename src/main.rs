eed74f74fc985a4f1cfd43a4947d9ee458909c49
rade <rade.salvador@gmail.com>
2026-07-12 16:49:51 +0200

    Hid the player on the main screen

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use bevy::{
-  camera::{ RenderTarget, visibility::RenderLayers },
-  input::InputPlugin,
-  prelude::*,
-  window::{ WindowRef, WindowResolution },
+    camera::{ RenderTarget, visibility::RenderLayers },
+    input::InputPlugin,
+    prelude::*,
+    window::{ WindowRef, WindowResolution },
 };
 use bevy_grid::*;
 
 use crate::{
-  input::OwnInputPlugin,
-  map::{ absolute_map::AbsoluteMapPlugin, relative_map::RelativeMapPlugin, * },
+    input::OwnInputPlugin,
+    map::{ absolute_map::AbsoluteMapPlugin, relative_map::RelativeMapPlugin, * },
 };
 use render::*;
 
@@ -23,44 +23,44 @@ const WINDOW_HEIGHT: u32 = 1080;
 const WALL_HEIGHT: f32 = 20.0;
 
 fn main() {
-  App::new()
-    .add_plugins(
-      DefaultPlugins.set(WindowPlugin {
-        primary_window: Some(Window {
-          title: "My Bevy App".to_string(),
-          resolution: WindowResolution::new(RAY_COUNT as u32, WINDOW_HEIGHT),
-          resizable: false,
-          ..default()
-        }),
-        ..default()
-      })
-    )
-    .add_systems(Startup, setup)
-    .add_systems(Startup, setup_gizmo_layers)
-    .add_plugins(OwnInputPlugin)
-    .add_plugins(AbsoluteMapPlugin)
-    .add_plugins(RelativeMapPlugin)
-    .init_state::<MapViewMode>()
-    .add_systems(Update, ray::get_hits)
-    .add_systems(Update, render)
-    .insert_resource(Map {
-      walls: vec![
-        Wall::new(-100.0, -100.0, 100.0, 100.0),
-        Wall::new(-100.0, 50.0, 100.0, 50.0),
-        Wall::new(-200.0, -200.0, -200.0, 200.0),
-        Wall::new(-200.0, 200.0, 200.0, 200.0),
-        Wall::new(-200.0, -200.0, 200.0, -200.0),
-        Wall::new(200.0, 200.0, 200.0, -200.0)
-      ],
-    })
-    .insert_resource(ViewInfo {
-      angle: 70.0,
-      max_distance: 500.0,
-      view_distance: 50.0,
-    })
-    .insert_resource(Hits::default())
-    .init_gizmo_group::<MapGizmos>()
-    .run();
+    App::new()
+        .add_plugins(
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "My Bevy App".to_string(),
+                    resolution: WindowResolution::new(RAY_COUNT as u32, WINDOW_HEIGHT),
+                    resizable: false,
+                    ..default()
+                }),
+                ..default()
+            })
+        )
+        .add_systems(Startup, setup)
+        .add_systems(Startup, setup_gizmo_layers)
+        .add_plugins(OwnInputPlugin)
+        .add_plugins(AbsoluteMapPlugin)
+        .add_plugins(RelativeMapPlugin)
+        .init_state::<MapViewMode>()
+        .add_systems(Update, ray::get_hits)
+        .add_systems(Update, render)
+        .insert_resource(Map {
+            walls: vec![
+                Wall::new(-100.0, -100.0, 100.0, 100.0),
+                Wall::new(-100.0, 50.0, 100.0, 50.0),
+                Wall::new(-200.0, -200.0, -200.0, 200.0),
+                Wall::new(-200.0, 200.0, 200.0, 200.0),
+                Wall::new(-200.0, -200.0, 200.0, -200.0),
+                Wall::new(200.0, 200.0, 200.0, -200.0)
+            ],
+        })
+        .insert_resource(ViewInfo {
+            angle: 70.0,
+            max_distance: 500.0,
+            view_distance: 50.0,
+        })
+        .insert_resource(Hits::default())
+        .init_gizmo_group::<MapGizmos>()
+        .run();
 }
 
 //Player marker
@@ -69,56 +69,51 @@ struct Player;
 
 #[derive(Resource)]
 pub struct ViewInfo {
-  pub angle: f32,
-  pub max_distance: f32,
-  //Distance which the screen sits from the players point of view
-  pub view_distance: f32,
+    pub angle: f32,
+    pub max_distance: f32,
+    //Distance which the screen sits from the players point of view
+    pub view_distance: f32,
 }
 
 #[derive(Resource)]
 struct Hits([Option<Vec2>; RAY_COUNT]);
 
 impl Default for Hits {
-  fn default() -> Self {
-    Hits([None; RAY_COUNT])
-  }
+    fn default() -> Self {
+        Hits([None; RAY_COUNT])
+    }
 }
 
 fn setup(
-  mut commands: Commands,
-  mut meshes: ResMut<Assets<Mesh>>,
-  mut materials: ResMut<Assets<ColorMaterial>>
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>
 ) {
-  commands.spawn(Camera2d);
+    commands.spawn(Camera2d);
 
-  //Spawn Map Window
-  let resolution: WindowResolution = (500, 500).into();
-  let window_size = Vec2::new(resolution.width(), resolution.height());
+    //Spawn Map Window
+    let resolution: WindowResolution = (500, 500).into();
+    let window_size = Vec2::new(resolution.width(), resolution.height());
 
-  let map_win = commands
-    .spawn((Window { resolution: resolution, resizable: false, ..default() }, MapWindowMarker))
-    .id();
+    let map_win = commands
+        .spawn((Window { resolution: resolution, resizable: false, ..default() }, MapWindowMarker))
+        .id();
 
-  let mut grid = Grid::new(GridSize { x: 8, y: 8 });
-  grid.build(window_size);
-  commands.insert_resource(grid);
+    let mut grid = Grid::new(GridSize { x: 8, y: 8 });
+    grid.build(window_size);
+    commands.insert_resource(grid);
 
-  //Spawn Map Camera
-  commands.spawn((
-    Camera2d,
-    RenderLayers::layer(1),
-    RenderTarget::Window(WindowRef::Entity(map_win)),
-  ));
+    //Spawn Map Camera
+    commands.spawn((
+        Camera2d,
+        RenderLayers::layer(1),
+        RenderTarget::Window(WindowRef::Entity(map_win)),
+    ));
 
-  commands.insert_resource(MapWindow { id: map_win });
+    commands.insert_resource(MapWindow { id: map_win });
 
-  //Spawn Map player
-  commands.spawn((
-    Player,
-    Transform::default(),
-    Mesh2d(meshes.add(Circle::new(10.0))),
-    MeshMaterial2d(materials.add(ColorMaterial::from(Color::WHITE))),
-  ));
+    //Spawn Map player
+    commands.spawn((Player, Transform::default()));
 }
 
 //Setup for different Gizmo configs
@@ -126,6 +121,6 @@ fn setup(
 pub struct MapGizmos;
 
 fn setup_gizmo_layers(mut config_store: ResMut<GizmoConfigStore>) {
-  let (config, _) = config_store.config_mut::<MapGizmos>();
-  config.render_layers = RenderLayers::layer(1);
+    let (config, _) = config_store.config_mut::<MapGizmos>();
+    config.render_layers = RenderLayers::layer(1);
 }
